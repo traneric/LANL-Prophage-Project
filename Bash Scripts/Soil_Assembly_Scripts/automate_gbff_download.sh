930df3195058e1531c7829038b72010bcdda91ed
traneric <extran@ucdavis.edu>
2021-03-15 22:40:36 -0700

    Add soil and marine scripts

---
+++ Bash Scripts/Soil_Assembly_Scripts/automate_gbff_download.sh
@@ -0,0 +1,19 @@
+# This program takes in a file of NCBI accession numbers and downloads its .gbff file from NCBI.
+
+cat ncbi_id_multi_chr.txt | while read ncbi_id
+do
+	wget "https://www.ncbi.nlm.nih.gov/search/all/?term=${ncbi_id}&database=assembly" -O 1.html
+
+	ASSEMBLY_LINK="$(grep "https://www.ncbi.nlm.nih.gov/assembly" 1.html | head -1 | sed 's/<a href="//' | sed 's/.$//')"
+
+	wget $ASSEMBLY_LINK -O 2.html
+
+	FTP_LINK="$(grep assembly_report 2.html | sed -E -e 's#.*href="(https://ftp.ncbi.nlm.nih.gov/genomes/all[^"]*).*#\1#' | sed -e 's#/[^/]*$##'
+)"
+
+	GENE_ID_NAME="$(echo $FTP_LINK | sed 's/.*\///')"
+	FASTA_LINK="${FTP_LINK}/${GENE_ID_NAME}_genomic.fna.gz"
+	GBFF_LINK="${FTP_LINK}/${GENE_ID_NAME}_genomic.gbff.gz"
+
+	wget $GBFF_LINK
+done
